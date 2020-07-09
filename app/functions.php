5760775a081245e6502c6bd56e08e9b7155138e1
Andrew Hartnett <ahartnett@insiderealestate.com>
2020-07-09 15:31:51 -0600

    initial commit

---
+++ app/functions.php
@@ -0,0 +1,19 @@
+<?php
+
+function humanMonth($num){
+  $months = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
+  ];
+  return $months[$num - 1];
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="flex items-center">
+        <div class="md:w-5/6 md:mx-auto">
+            <example></example>
+        </div>
+    </div>
+@endsection
